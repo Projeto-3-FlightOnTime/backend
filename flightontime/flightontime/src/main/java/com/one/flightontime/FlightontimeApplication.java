0e4ee2051d71df89c7255bae1567e8cd637fba43
VictorDevvs <victorsantosdevvs@gmail.com>
2025-12-11 08:15:50 -0300

    Adicionando a anotação @EnableFeignClients na classe principal

--- flightontime/flightontime/src/main/java/com/one/flightontime/FlightontimeApplication.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/FlightontimeApplication.java
@@ -2,8 +2,10 @@ package com.one.flightontime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FlightontimeApplication {
 
 	public static void main(String[] args) {
