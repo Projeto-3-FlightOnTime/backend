5cf23c714c7de6a1c50e6d3d635932349a7b190a
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-12-12 13:19:22 -0300

    Criação de Entity e Enum

--- flightontime/flightontime/src/main/java/com/one/flightontime/domain/HistoricoPrevisao.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/domain/HistoricoPrevisao.java
@@ -1,5 +1,46 @@
 package com.one.flightontime.domain;
 
+import com.one.flightontime.domain.enums.StatusPredicao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "HistoricoPrevisao")
 public class HistoricoPrevisao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idHistorico;
+
+    private String codCompanhia;
+
+    private String codAeroportoOrigem;
+
+    private String codAeroportoDestino;
+
+    private LocalDateTime dataHoraPartida;
+
+    private Double distanciaKm;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPredicao statusPredicao;
+
+    private Double probabilidade;
+
+    private LocalDateTime dataConsulta;
+
+    @PrePersist
+    private void preencherData(){
+        this.dataConsulta = LocalDateTime.now();
+    }
+
 }
