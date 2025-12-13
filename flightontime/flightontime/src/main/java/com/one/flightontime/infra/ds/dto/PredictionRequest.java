026722ad5175beba0b3321572a33a742da97d043
Ayran <ayranvieira29@gmail.com>
2025-12-12 22:02:49 -0300

    Atualizando DTOs para record e adicionando campos conforme documentacao

--- flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/dto/PredictionRequest.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/dto/PredictionRequest.java
@@ -1,11 +1,23 @@
 package com.one.flightontime.infra.ds.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
-@Data
-public class PredictionRequest {
-    //AQUI VAMOS TER QUE AGUARDAR A EQUIPE DE DS PARA COLOCARMOS QUAIS VAO SER AS CHAMADAS, MAS POR ENQUANTO VOU DEIXAR ESSES 2 DE EXEMPLO
-    private Long id;
-    private String text;
-    private Double number;
-}
+import java.time.LocalDateTime;
+
+@Builder
+public record PredictionRequest(
+
+        @JsonProperty("cod_companhia")
+        String codCompanhia,
+        @JsonProperty("cod_aeroporto_origem")
+        String codAeroportoOrigem,
+        @JsonProperty("cod_aeroporto_destino")
+        String codAeroportoDestino,
+        @JsonProperty("data_hora_partida")
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime dataHoraPartida,
+        @JsonProperty("distancia_km")
+        Integer distanciaKm
+) {}
