ff00864bea2bb602a63045cd4f22b35ebe32d7db
VictorDevvs <victorsantosdevvs@gmail.com>
2025-12-22 09:09:15 -0300

    Adicionando validacao de data e horário

--- flightontime/flightontime/src/main/java/com/one/flightontime/service/validations/ValidationPrediction.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/service/validations/ValidationPrediction.java
@@ -1,8 +1,11 @@
 package com.one.flightontime.service.validations;
 
 import com.one.flightontime.infra.ds.dto.PredictionRequest;
+import com.one.flightontime.infra.exceptions.DataHoraPartidaInvalidaException;
 import com.one.flightontime.infra.exceptions.OrigemDestinoException;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class ValidationPrediction {
@@ -10,6 +13,16 @@ public class ValidationPrediction {
     public void validateOrigemDestino(PredictionRequest aeroporto1){
         if(aeroporto1.codAeroportoOrigem().equals(aeroporto1.codAeroportoDestino())){
            throw new OrigemDestinoException("O aeroporto de origem não pode ser igual ao de destino");
+        }
+
+        if(aeroporto1.dataHoraPartida().isBefore(LocalDateTime.now().minusMinutes(1))){
+            throw new DataHoraPartidaInvalidaException("A data e hora de partida não pode estar no passado");
+        }
+
+        if(aeroporto1.dataHoraPartida().isAfter(LocalDateTime.now().plusDays(365))){
+            throw new DataHoraPartidaInvalidaException(
+                    "A data e hora de partida não pode ser maior que um ano a partir da data atual"
+            );
         }
     }
 
