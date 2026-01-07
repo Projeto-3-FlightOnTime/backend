042845a994075cbb7d6d71a5dc5abba443dab5fd
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2026-01-07 12:12:17 -0300

    Documentação da API detalhada, feita com Swagger

--- flightontime/flightontime/src/main/java/com/one/flightontime/controllers/RequestController.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/controllers/RequestController.java
@@ -13,16 +13,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/predict")
 @Validated
 @Slf4j
+
+@Tag(name = "Predição de atrasos", description = "Endpoint para análise de risco de voos")
 public class RequestController {
 
     private final HistoricoService historicoService;
 
+    @Operation(summary = "Calcular probabilidade de atraso",
+            description = "Recebe os dados do voo e realiza uma requisição interna para " +
+                    "a API Python(Data Science), que processa o modelo de Machine Learning e retorna a previsão. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Predição realizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos(Data no passado, sigla errada ou campos vazios"),
+            @ApiResponse(responseCode = "500", description = "Erro de comunicação com o serviço Python")
+    })
     @PostMapping
     public ResponseEntity<@NonNull PredictionResponse> receberDadosApi(@RequestBody @Valid PredictionRequest dados){
         PredictionResponse enviarDados = historicoService.prediction(dados);
