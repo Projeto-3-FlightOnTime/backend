cde0bbf743d08f11c9b2c98436d0e9b378d7031c
Ayran <ayranvieira29@gmail.com>
2026-01-19 22:45:39 -0300

    Alteracoes para deploy

--- flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "dsClient", url = "http://127.0.0.1:8000")
+@FeignClient(name = "dsClient", url = "${DS_CLIENT_URL}")
 public interface DsClient {
 
     @PostMapping("/predict")
